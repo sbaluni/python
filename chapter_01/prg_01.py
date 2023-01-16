6c634f1265a2200455367ec0b6a8e3c30c7cef2a
Suryakant Baluni <sbaluni1989@gmail.com>
2023-01-17 00:32:28 +0530

    Modified description.

--- chapter_01/prg_01.py
+++ chapter_01/prg_01.py
@@ -2,7 +2,7 @@
 #
 #   User                    Date(dd/mm/yyyy)     Description 
 #   ------------------------------------------------------------------------------------------
-#   Suryakant Baluni        02/08/2022           Python program to print "Hello, world!"
+#   Suryakant Baluni        02/08/2022           Python program to print "Hello, world!" and quoted strings.
 #
 
 print("Hello, world!")
