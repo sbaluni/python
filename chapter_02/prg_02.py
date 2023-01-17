85a990dc6bc7e1e701af88f105af9d5bb0c4eb02
Suryakant Baluni <sbaluni1989@gmail.com>
2023-01-17 21:38:44 +0530

    added ch 02 prg_02.py

---
+++ chapter_02/prg_02.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+#
+#   User                    Date(dd/mm/yyyy)     Description 
+#   ------------------------------------------------------------------------------------------
+#   Suryakant Baluni        17/01/2023           Input a number from(1-12) as month, and display
+#                                                corresponding month name in english.
+
+month = int(input("Input month(MM fromat 1-12):"))
+
+if month == 1:
+    print("January")
+elif month == 2:
+    print("February")
+elif month == 3:
+    print("March")
+elif month == 4:
+    print("April")
+elif month == 5:
+    print("May")
+elif month == 6:
+    print("June")
+elif month == 7:
+    print("July")
+elif month == 8:
+    print("August")
+elif month == 9:
+    print("September")
+elif month == 10:
+    print("October")
+elif month == 11:
+    print("November")
+elif month == 12:
+    print("December")
+else:
+    print("Not a valid month!")
