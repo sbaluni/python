3098833c01a64df8f74b8564a5d7f8aa30f52b22
Suryakant Baluni <sbaluni1989@gmail.com>
2023-01-17 21:28:31 +0530

    Added chapter 02

--- chapter_01/prg_08.py
+++ chapter_01/prg_08.py
@@ -3,7 +3,7 @@
 #   User                    Date(dd/mm/yyyy)     Description 
 #   ------------------------------------------------------------------------------------------
 #   Suryakant Baluni        17/01/2023           Input radius of circle from user and calculate 
-#                                                area and perimeter of circle. 
+#                                                area and perimeter of circle up to 2 decimal places. 
 #
 
 r = float(input("Input radius of circle:"))
@@ -17,6 +17,11 @@ area = pi * r * r
 # Calculate perimeter of rectangle
 p = 2 * pi * r
 
-# Print results.
-print("Area of circle: ", area)
-print("Perimeter of circle: ", p)
+# Print results. Use formatting options to format output up to 2 decimal places.
+# This is useful when output is something like 3.1, but we want to print 3.10
+print("Area of circle: %.2f" % area)
+print("Perimeter of circle: %.2f" % p)
+
+# We can also use formatting syntax like this.
+print("Area of circle: " + "{:.2f}".format(area))
+print("Perimeter of circle: " +  "{:.2f}".format(p))
---
+++ chapter_02/prg_01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+#
+#   User                    Date(dd/mm/yyyy)     Description 
+#   ------------------------------------------------------------------------------------------
+#   Suryakant Baluni        17/01/2023           Input a number from user and check if the number is even or odd.
+
+num = int(input("Input a number:"))
+
+if num % 2 == 0:
+    print("%d is Even." % num)
+else:
+    print("%d is Odd." % num)
