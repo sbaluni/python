d0082c762f548d887d016b66a483c686e35085a1
Suryakant Baluni <sbaluni1989@gmail.com>
2023-07-20 22:59:43 +0530

    commit

--- chapter_01(Basics)/prg_04_basic_arithmatic_operations_on_float.py
+++ chapter_01(Basics)/prg_04_basic_arithmatic_operations_on_float.py
@@ -8,7 +8,7 @@
 a = float(input("Input number a:"))
 b = float(input("Input number b:"))
 
-# Calculate sum and store result in a variable c
+# Perform various arithmatic operations and store results in individual variables.
 sum = a + b
 diff = a - b
 multi = a*b
